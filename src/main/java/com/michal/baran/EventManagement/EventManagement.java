aed9d87ae4bf3235fc45234e3df126dd4ef1bd57
Michal Baran <michal.baran.dev@gmail.com>
2023-04-01 16:26:25 +0200

    Initial commit

---
+++ src/main/java/com/michal/baran/EventManagement/EventManagement.java
@@ -0,0 +1,13 @@
+package com.michal.baran.EventManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagement.class, args);
+	}
+
+}
